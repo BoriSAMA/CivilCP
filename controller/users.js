6892927941be2c6ae8fd8537fa01934e5d6f7a95
BoriSAMA <andremur98@gmail.com>
2021-01-27 12:13:51 -0500

    Dummy files added to make the file herarchy

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,54 @@
 const express = require('express');
 const router = express.Router();
-var initModels = require("../models/init-models");
+//var initModels = require("../models/init-models");
+var User = require('../models/user')(sequelize, DataTypes);
 
-var models = initModels(sequelize);
 
-models.User.findAll({ where: { username: "tony" }}).then(...);
+// todos los modelos
+// var models = initModels(sequelize);
+
+//login
+router.post('/login', async(req, res) => {
+	try{
+		await User.findOne({ user_email: req.body.email },
+			function (err, user) {
+				if (err) {
+					res.status(500).json({
+						ok: false,
+						err: {
+							message: "Usuario o contraseña incorrectos"
+						},
+						user: user
+					})
+				}
+				// Verifica que exista un usuario con el mail escrita por el usuario.
+				if (user == null) {
+					res.status(400).json({
+						ok: false,
+						err: {
+							message: "Usuario o contraseña incorrectos"
+						}
+					});
+				}
+				// Valida que la contraseña escrita por el usuario, sea la almacenada en la db
+				else if (req.body.password != user.user_password){
+					res.status(400).json({
+						ok: false,
+						err: {
+							message: "Usuario o contraseña incorrectos"
+						}
+					});
+				}
+				else{
+					res.status(200).json({
+						ok: true,
+						usuario: user
+					})
+				}
+			}	
+		);
+
+	}catch(err){
+		res.status(500).json({message:err});
+	}
+});
